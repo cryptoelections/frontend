022225efcbe058f18786dd0400aaa3e9f4031d68
Darya Baklanova <darya.baklanova@gmail.com>
2018-03-01 00:50:53 +0700

    back

--- src/app/shared/ngrx/city/city.effects.ts
+++ src/app/shared/ngrx/city/city.effects.ts
@@ -27,10 +27,9 @@ export class CityEffects {
   @Effect()
   loadDynamicCityInformation$ = this.actions$
     .ofType(city.LOAD_DYNAMIC_CITY_INFORMATION_REQUEST)
-    .switchMap((action: city.LoadDynamicCityInformationRequest) => Observable.interval(60000)
-      .switchMap(() => this.cityService.getDynamic()
+    .switchMap((action: city.LoadDynamicCityInformationRequest) => this.cityService.getDynamic()
         .map((list: { [id: string]: Partial<City> }) => new city.LoadDynamicCityInformationResponse(list))
-        .catch((error) => Observable.of(new city.LoadDynamicCityInformationResponse({})))));
+        .catch((error) => Observable.of(new city.LoadDynamicCityInformationResponse({}))));
 
 
   @Effect()
